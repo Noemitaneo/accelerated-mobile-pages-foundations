a6ea76cf29b4c394c32d509038e110cec3d34f15
Noemi Taneo <62083474+Noemitaneo@users.noreply.github.com>
2022-11-09 14:35:11 +0800

    update img

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const ampPlugin = require('@ampproject/eleventy-plugin-amp');
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(ampPlugin);
-  eleventyConfig.addPassthroughCopy("./src/_inc/img");
+  eleventyConfig.addPassthroughCopy("src/_inc/img/mountains.jpg");
   return {
     dir: {
       input: "src",

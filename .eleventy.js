c18b759273bc7ce93834889518e885f0e34e2557
Noemi Taneo <62083474+Noemitaneo@users.noreply.github.com>
2022-11-09 14:29:57 +0800

    update js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const ampPlugin = require('@ampproject/eleventy-plugin-amp');
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(ampPlugin);
-  eleventyConfig.addPassthroughCopy("./src/_inc");
+  eleventyConfig.addPassthroughCopy("./src/_inc/");
   return {
     dir: {
       input: "src",
